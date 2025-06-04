33b7b6b00de5229fcfd6cca5a9b2e30779eeb5b1
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-06-04 23:30:54 +0530

    feat: Add helper to send and verify token using REST api

---
+++ src/firebase/helper.js
@@ -0,0 +1,24 @@
+/**
+ * Sends a token to the server for verification.
+ *
+ * @param {*} token - The token to be sent for verification.
+ *
+ * @returns {Promise<void>} A promise that resolves when the token is successfully verified.
+ */
+const sendAndVerifyToken = async (token) => {
+    const res = await fetch('/wp-json/firebase-auth/v1/verify-token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }),
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to verify token');
+    }
+
+    console.log(res);
+};
+
+export { sendAndVerifyToken };
